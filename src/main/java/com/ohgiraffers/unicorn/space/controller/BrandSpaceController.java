b3193ad50d118595bb6d4a6c7b928263967a760e
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2024-10-28 18:00:57 +0900

    [Feat] fix url

--- src/main/java/com/ohgiraffers/unicorn/space/controller/BrandSpaceController.java
+++ src/main/java/com/ohgiraffers/unicorn/space/controller/BrandSpaceController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/place/{corpId}")
+@RequestMapping("/api/v1/space/{corpId}")
 public class BrandSpaceController {
 
     @Autowired
