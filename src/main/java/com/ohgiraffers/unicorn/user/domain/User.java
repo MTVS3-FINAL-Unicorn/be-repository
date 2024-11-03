981931b71464f9f6a626a4f958d79e38df27ff19
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2024-11-03 23:46:25 +0900

    [Feat] Add User Entity

---
+++ src/main/java/com/ohgiraffers/unicorn/user/domain/User.java
@@ -0,0 +1,49 @@
+package com.ohgiraffers.unicorn.user.domain;
+
+import com.ohgiraffers.unicorn.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int age;
+
+    @Column(nullable = false)
+    private String gender;
+
+    @Enumerated(value = EnumType.STRING)
+    @ColumnDefault("'USER'")
+    private Authority authority;
+
+    @Builder
+    public User(String email, String password, String name, int age, String gender, Authority authority) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.authority = authority;
+    }
+
+}
