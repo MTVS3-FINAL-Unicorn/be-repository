cb4c7932f6dd292a49575f7bdf9f903635c7767f
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2024-11-03 23:46:00 +0900

    [Feat] Security, JWT 파일 추가

---
+++ src/main/java/com/ohgiraffers/unicorn/config/SecurityConfig.java
@@ -0,0 +1,75 @@
+package com.ohgiraffers.unicorn.config;
+
+import com.ohgiraffers.unicorn.error.exception.Exception401;
+import com.ohgiraffers.unicorn.error.exception.Exception403;
+import com.ohgiraffers.unicorn.jwt.JWTTokenFilter;
+import com.ohgiraffers.unicorn.jwt.JWTTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JWTTokenProvider jwtTokenProvider;
+
+    private static final String[] WHITE_LIST = {
+            "/api/**",
+            "/h2-console/**"  // h2-console 경로 추가
+    };
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement((sessionManagement) ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests((request) -> request
+                        .requestMatchers(WHITE_LIST).permitAll()  // antMatchers를 사용해 화이트리스트 경로 허용
+                        .anyRequest().authenticated())
+                .exceptionHandling(exception -> {
+                    exception.authenticationEntryPoint(authenticationEntryPoint());
+                    exception.accessDeniedHandler(accessDeniedHandler());
+                })
+                .addFilterBefore(new JWTTokenFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
+        // Spring Security Custom Filter 적용 - Form '인증'에 대해서 적용
+
+        return httpSecurity.build();
+    }
+
+    private AuthenticationEntryPoint authenticationEntryPoint() {
+        return (request, response, authException) -> {
+            throw new Exception401("Authentication failed: " + authException.getMessage());
+        };
+    }
+
+    private AccessDeniedHandler accessDeniedHandler() {
+        return (request, response, accessDeniedException) -> {
+            throw new Exception403("Access denied: " + accessDeniedException.getMessage());
+        };
+    }
+}
---
+++ src/main/java/com/ohgiraffers/unicorn/user/domain/Authority.java
@@ -0,0 +1,5 @@
+package com.ohgiraffers.unicorn.user.domain;
+
+public enum Authority {
+    ADMIN, USER, GUEST, NONE;
+}
