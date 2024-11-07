82c1816d2a77428080af99d2551928826ec5cc8b
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2024-11-08 08:49:18 +0900

    [Refactor] 참가자 신청 상태값 수정

--- src/main/java/com/ohgiraffers/unicorn/meeting/entity/ParticipantStatus.java
+++ src/main/java/com/ohgiraffers/unicorn/meeting/entity/ParticipantStatus.java
@@ -1,5 +1,5 @@
 package com.ohgiraffers.unicorn.meeting.entity;
 
 public enum ParticipantStatus {
-    PENDING, APPROVED, CANCELLED_PENDING, CANCELLED_CONFIRMED, REJECTED
+    PENDING, APPROVED, CANCELLED, REJECTED
 }
