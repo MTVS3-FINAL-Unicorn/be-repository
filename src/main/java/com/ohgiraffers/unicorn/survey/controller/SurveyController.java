cae999b73d3e50c49defabd7658e0b1cdf9110ea
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2024-11-09 21:29:45 +0900

    [Feat] 답변 저장 api 추가(텍스트형)

--- src/main/java/com/ohgiraffers/unicorn/survey/controller/SurveyController.java
+++ src/main/java/com/ohgiraffers/unicorn/survey/controller/SurveyController.java
@@ -1,11 +1,15 @@
 package com.ohgiraffers.unicorn.survey.controller;
 
 import com.ohgiraffers.unicorn.survey.dto.QuestionDTO;
+import com.ohgiraffers.unicorn.survey.entity.Answer;
 import com.ohgiraffers.unicorn.survey.entity.Question;
+import com.ohgiraffers.unicorn.survey.service.AnswerService;
 import com.ohgiraffers.unicorn.survey.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import static com.ohgiraffers.unicorn.utils.SecurityUtils.getCurrentUserId;
 
 @RestController
 @RequestMapping("/api/v1/survey")
@@ -13,6 +17,9 @@ public class SurveyController {
 
     @Autowired
     private QuestionService questionService;
+
+    @Autowired
+    private AnswerService answerService;
 
     @PostMapping("/question")
     public ResponseEntity<Question> createQuestion(@RequestBody QuestionDTO questionRequest) {
@@ -30,4 +37,12 @@ public class SurveyController {
         return ResponseEntity.ok(question);
     }
 
+    @PostMapping("/answers/text")
+    public ResponseEntity<Answer> saveTextAnswer(
+            @RequestParam Long questionId,
+            @RequestParam String content) {
+        Long indivId = getCurrentUserId();
+        Answer answer = answerService.saveAnswer(questionId, indivId, content);
+        return ResponseEntity.ok(answer);
+    }
 }
