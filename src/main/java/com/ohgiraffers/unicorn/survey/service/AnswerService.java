bfaf34652d9829c0f7cd2af2423790bbf179a901
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2024-11-09 21:29:52 +0900

    [Feat] 답변 저장 기능 추가(텍스트형)

---
+++ src/main/java/com/ohgiraffers/unicorn/survey/service/AnswerService.java
@@ -0,0 +1,21 @@
+package com.ohgiraffers.unicorn.survey.service;
+
+import com.ohgiraffers.unicorn.survey.entity.Answer;
+import com.ohgiraffers.unicorn.survey.repository.AnswerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AnswerService {
+
+    @Autowired
+    private AnswerRepository answerRepository;
+
+    public Answer saveAnswer(Long questionId, Long indivId, String content) {
+        Answer answer = new Answer();
+        answer.setQuestionId(questionId);
+        answer.setIndivId(indivId);
+        answer.setContent(content);
+        return answerRepository.save(answer);
+    }
+}
