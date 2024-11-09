91038ad92001fed9af412f14a68fef40a77edff0
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2024-11-09 21:16:44 +0900

    [Feat] add SurveyController

---
+++ src/main/java/com/ohgiraffers/unicorn/survey/controller/SurveyController.java
@@ -0,0 +1,28 @@
+package com.ohgiraffers.unicorn.survey.controller;
+
+import com.ohgiraffers.unicorn.survey.dto.QuestionDTO;
+import com.ohgiraffers.unicorn.survey.entity.Question;
+import com.ohgiraffers.unicorn.survey.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/survey")
+public class SurveyController {
+
+    @Autowired
+    private QuestionService questionService;
+
+    @PostMapping("/question")
+    public ResponseEntity<Question> createQuestion(@RequestBody QuestionDTO questionRequest) {
+        Question question = questionService.createQuestion(
+                questionRequest.getMeetingId(),
+                questionRequest.getContent(),
+                questionRequest.getType()
+        );
+        return ResponseEntity.ok(question);
+    }
+
+
+}
