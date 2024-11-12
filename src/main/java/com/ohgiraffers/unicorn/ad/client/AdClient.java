4f18677c165e765870e2bcaca36b8c00488bde3e
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2024-11-12 17:02:00 +0900

    [Refactor] adClient url 수정

--- src/main/java/com/ohgiraffers/unicorn/ad/client/AdClient.java
+++ src/main/java/com/ohgiraffers/unicorn/ad/client/AdClient.java
@@ -1,9 +1,13 @@
-//package com.ohgiraffers.unicorn.ad.client;
-//
-//import org.springframework.cloud.openfeign.FeignClient;
-//
-//@FeignClient(name = "generating-ad", url = "http://metaai2.iptime.org:7777")
-//public interface AdClient {
-//
-//
-//}
+package com.ohgiraffers.unicorn.ad.client;
+
+import com.ohgiraffers.unicorn.ad.dto.AdRequestDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "generating-ad", url = "http://metaai2.iptime.org:7777")
+public interface AdClient {
+
+    @PostMapping("/generate-videoad")
+    String generateVideoAd(@RequestBody AdRequestDTO request);
+}
