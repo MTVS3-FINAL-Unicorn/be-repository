36ed75c3fc5ecfaa061137e49ea4790ad2ac732e
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2024-11-13 15:17:15 +0900

    [Refactor] 보고서 결과 조회 repository 수정

--- src/main/java/com/ohgiraffers/unicorn/report/repository/ReportRepository.java
+++ src/main/java/com/ohgiraffers/unicorn/report/repository/ReportRepository.java
@@ -2,10 +2,15 @@ package com.ohgiraffers.unicorn.report.repository;
 
 import com.ohgiraffers.unicorn.report.entity.Report;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ReportRepository extends JpaRepository<Report, Long> {
-    List<Report> findByMeetingId(Long meetingId);
+    @Query("SELECT r FROM Report r WHERE r.meetingId = :meetingId AND r.questionId IS NULL")
+    List<Report> findReportsByMeetingIdAndNullQuestionId(@Param("meetingId") Long meetingId);
+
+    List<Report> findByQuestionId(Long questionId);
 }
 
