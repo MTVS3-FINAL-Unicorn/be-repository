480faab30a03162007da24155e67995278182a48
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2024-11-24 17:42:38 +0900

    [Deploy] EnableAspectJAutoProxy 추가

--- src/main/java/com/ohgiraffers/unicorn/UnicornApplication.java
+++ src/main/java/com/ohgiraffers/unicorn/UnicornApplication.java
@@ -2,6 +2,7 @@ package com.ohgiraffers.unicorn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -10,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EnableJpaAuditing
 @EnableFeignClients
 @EnableAsync
+@EnableAspectJAutoProxy
 public class UnicornApplication {
 
     public static void main(String[] args) {
