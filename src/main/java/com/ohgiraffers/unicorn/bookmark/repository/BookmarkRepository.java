0d8107eb76d7c6f8ffce458ea7921ef1cd80b915
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2024-12-08 01:41:00 -0800

    [Feat] indivId로 bookmark 추가한 corpProfile 조회 기능 추가

--- src/main/java/com/ohgiraffers/unicorn/bookmark/repository/BookmarkRepository.java
+++ src/main/java/com/ohgiraffers/unicorn/bookmark/repository/BookmarkRepository.java
@@ -2,9 +2,16 @@ package com.ohgiraffers.unicorn.bookmark.repository;
 
 import com.ohgiraffers.unicorn.bookmark.entity.Bookmark;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
+
+    @Query("SELECT b.corpId FROM Bookmark b WHERE b.indivId = :indivId")
+    List<Long> findCorpIdsByIndivId(@Param("indivId") Long indivId);
 
 }
