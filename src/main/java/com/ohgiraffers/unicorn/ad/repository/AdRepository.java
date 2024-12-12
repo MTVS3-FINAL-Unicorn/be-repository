630a257a8e7aac6c3d17a7e01cc8af71fc1cf0a4
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2024-12-12 13:44:21 +0900

    [Refactor] 광고 조회 로직 수정

--- src/main/java/com/ohgiraffers/unicorn/ad/repository/AdRepository.java
+++ src/main/java/com/ohgiraffers/unicorn/ad/repository/AdRepository.java
@@ -4,10 +4,11 @@ import com.ohgiraffers.unicorn.ad.entity.Ad;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface AdRepository extends JpaRepository<Ad, Long> {
-    Optional<Ad> findByAdIdAndIsOpened(Long adId, int isOpened);
-    Optional<Ad> findByCorpId(Long corpId);
+    Optional<Ad> findByAdId(Long adId);
+    List<Ad> findByCorpId(Long corpId); // 기업의 모든 광고 조회
 }
