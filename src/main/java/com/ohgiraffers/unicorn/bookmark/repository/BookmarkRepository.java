e65caa9975cb4e9c7743fc0a6ae9f835c69a1f98
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2024-12-12 11:30:41 +0900

    [Feat] 즐겨찾기한 고객 조회 로직 구현

--- src/main/java/com/ohgiraffers/unicorn/bookmark/repository/BookmarkRepository.java
+++ src/main/java/com/ohgiraffers/unicorn/bookmark/repository/BookmarkRepository.java
@@ -1,5 +1,6 @@
 package com.ohgiraffers.unicorn.bookmark.repository;
 
+import com.ohgiraffers.unicorn.auth.entity.Indiv;
 import com.ohgiraffers.unicorn.bookmark.entity.Bookmark;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -14,4 +15,6 @@ public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
     @Query("SELECT b.corpId FROM Bookmark b WHERE b.indivId = :indivId")
     List<Long> findCorpIdsByIndivId(@Param("indivId") Long indivId);
 
+    @Query("SELECT b.indivId FROM Bookmark b WHERE b.corpId = :corpId")
+    List<Long> findIndivIdByCorpId(@Param("corpId") Long corpId);
 }
