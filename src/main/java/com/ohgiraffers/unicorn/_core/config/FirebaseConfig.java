3c6d3543797a3e18ce7b0f0917838c571c902c89
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2024-12-15 11:19:36 +0900

    [Feat] add FirebaseConfig

---
+++ src/main/java/com/ohgiraffers/unicorn/_core/config/FirebaseConfig.java
@@ -0,0 +1,33 @@
+package com.ohgiraffers.unicorn._core.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Configuration
+public class FirebaseConfig {
+
+    @PostConstruct
+    public void initialize() {
+        try {
+            FileInputStream serviceAccount = new FileInputStream("src/main/resources/serviceAccountKey.json");
+
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setStorageBucket("unicorn-4b430.firebasestorage.app")
+                    .build();
+
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Firebase initialization failed", e);
+        }
+    }
+}
+
